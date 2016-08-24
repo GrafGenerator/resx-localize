06eb1906a0b8fb2b4aefb29642ff991ff71aa0d6
Nikita Ivanov <grafgenerator@gmail.com>
2016-08-24 14:39:47 +0700

    info

--- src/GrafGenerator.ResxLocalize.Tests/Properties/AssemblyInfo.cs
+++ src/GrafGenerator.ResxLocalize.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GrafGenerator.ResxLocalize.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 Nikita Ivanov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
