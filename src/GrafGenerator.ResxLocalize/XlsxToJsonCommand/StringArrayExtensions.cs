ca7c6bcf47d2155310882beeb81a284df24ebdb8
Nikita Ivanov <grafgenerator@gmail.com>
2016-08-24 16:11:04 +0700

    xlsx reading

---
+++ src/GrafGenerator.ResxLocalize/XlsxToJsonCommand/StringArrayExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace GrafGenerator.ResxLocalize.XlsxToJsonCommand
+{
+    public static class StringArrayExtensions
+    {
+        public static bool HasValues(this string[] array)
+        {
+            return array.Any(s => !string.IsNullOrEmpty(s));
+        }
+    }
+}
--- src/GrafGenerator.ResxLocalize/XlsxToJsonCommand/XlsxToJsonCommand.cs
+++ src/GrafGenerator.ResxLocalize/XlsxToJsonCommand/XlsxToJsonCommand.cs
@@ -1,5 +1,9 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Management.Automation;
+using ClosedXML.Excel;
+using Newtonsoft.Json.Linq;
 
 namespace GrafGenerator.ResxLocalize.XlsxToJsonCommand
 {
@@ -20,14 +24,85 @@ namespace GrafGenerator.ResxLocalize.XlsxToJsonCommand
         {
             if (!File.Exists(SourceXlsxPath))
             {
-                ThrowTerminatingError(new ErrorRecord(new FileNotFoundException("Input XLSX file not found.", SourceXlsxPath),
-                    "FileNotFound", ErrorCategory.InvalidArgument, SourceXlsxPath));
+                ThrowTerminatingError(
+                    new ErrorRecord(new FileNotFoundException("Input XLSX file not found.", SourceXlsxPath),
+                        "FileNotFound", ErrorCategory.InvalidArgument, SourceXlsxPath));
             }
         }
 
         protected override void ProcessRecord()
         {
+            var workbook = new XLWorkbook(SourceXlsxPath);
+            var strings = workbook.Worksheets.Select(GatherStringsFromWorksheet).SelectMany(w => w);
 
+            var array = new JArray();
+
+            foreach (var s in strings)
+            {
+                var o = new JObject();
+
+                foreach (var kv in s)
+                {
+                    o.Add(kv.Key, new JValue(kv.Value));
+                }
+
+                array.Add(o);
+            }
+
+            var fileName = Path.GetFileNameWithoutExtension(SourceXlsxPath);
+            var outputPath = fileName + ".strings.json";
+
+            File.WriteAllText(outputPath, array.ToString());
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private Dictionary<string, string>[] GatherStringsFromWorksheet(IXLWorksheet worksheet)
+        {
+            var keys = GetKeys(worksheet).ToArray();
+            var keysCount = keys.Length;
+            var rows = new List<string[]>();
+            var currentRow = 1;
+
+            var row = GetRow(worksheet, currentRow, keysCount).ToArray();
+
+            while (row.HasValues())
+            {
+                rows.Add(row);
+                currentRow++;
+            }
+
+            return rows
+                .Select(a => a
+                    .Select((c, i) => new {Key = keys[i], Value = c})
+                    .ToDictionary(r => r.Key, r => r.Value)
+                )
+                .ToArray();
+        }
+
+        private IEnumerable<string> GetKeys(IXLWorksheet worksheet)
+        {
+            var cell = worksheet.Cell(0, 0);
+            while (!string.IsNullOrEmpty(cell.Value.ToString()))
+            {
+                yield return cell.Value.ToString();
+                cell = cell.CellRight();
+            }
+        }
+
+        private IEnumerable<string> GetRow(IXLWorksheet worksheet, int row, int cellCount)
+        {
+            var cell = worksheet.Cell(row, 0);
+            var i = cellCount;
+
+            while (i > 0)
+            {
+                yield return cell.Value.ToString();
+                cell = cell.CellRight();
+                i--;
+            }
         }
 
         #endregion
